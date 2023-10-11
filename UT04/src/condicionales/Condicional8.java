35f2142d25265b2e91850277d86a913602e8a1a5
Natasha <nars.0596@gmail.com>
2023-10-11 19:07:23 +0200

    Cambios para revisar repo

--- UT04/src/condicionales/Condicional8.java
+++ UT04/src/condicionales/Condicional8.java
@@ -16,22 +16,37 @@ public class Condicional8 {
 		int numero3 = scanner.nextInt();
 		
 
-		{
-			if ((numero1 >= numero2) && (numero1 >=numero3) && (numero2 >= numero3)) 
-			System.out.printf("El orden de los numeros es %d, %d, %d", numero1, numero2, numero3);	
-		 else if  ((numero1 >= numero2) && (numero1 >=numero3) 
-				&& (numero1 < numero3)) 
-			System.out.printf("El orden de los numeros es %d, %d, %d", numero1, numero2, numero3);	
-		else if ((numero3 >= numero1) && (numero3 >=numero2) 
-				&& (numero2 < numero1)) 
+		
+		if ((numero1 >= numero2) && (numero1 >=numero3) && (numero2 >= numero3)) {
 			System.out.printf("El orden de los numeros es %d, %d, %d", numero1, numero2, numero3);
-		else if ((numero3 >= numero1) && (numero3 >=numero2) 
-				&& (numero2 < numero1)) 
+		} else if ((numero2 >= numero1) && (numero2 >= numero3) && (numero3 >= numero1)) {
+			System.out.printf("El orden de los numeros es %d, %d, %d", numero2, numero3, numero1);
+		} else if ((numero3 >= numero1) && (numero3 >= numero2) && (numero2 >= numero1)){
+			System.out.printf("El orden de los numeros es %d, %d, %d", numero3, numero2, numero1);
+		} else if ((numero1 < numero2) && (numero1 < numero3) && (numero2 < numero3)) {
 			System.out.printf("El orden de los numeros es %d, %d, %d", numero1, numero2, numero3);
-			
+		} else if ((numero2 < numero1) && (numero2 < numero3) && (numero3 < numero1)) {
+			System.out.printf("El orden de los numeros es %d, %d, %d", numero1, numero3, numero2);
+		} else if ((numero3 >= numero1) && (numero3 >= numero2) && (numero1 >= numero2)) {
+			System.out.printf("El orden de los numeros es %d, %d, %d", numero3, numero1, numero2);
+		}
 	}
-			
-  }
 }
+//		} else if  ((numero2 >= numero1) && (numero2 >=numero3) 
+//				&& (numero1 < numero3)) 
+//			System.out.printf("El orden de los numeros es %d, %d, %d", numero1, numero2, numero3);	
+//		else if ((numero3 >= numero1) && (numero3 >=numero2) 
+//				&& (numero2 < numero1)) 
+//			System.out.printf("El orden de los numeros es %d, %d, %d", numero1, numero2, numero3);
+//		else if ((numero3 >= numero1) && (numero3 >=numero2) 
+//				&& (numero2 < numero1)) 
+//			System.out.printf("El orden de los numeros es %d, %d, %d", numero1, numero2, numero3);
+//			
+//			}
+//		}
+//			
+//}
+
+
 
 
