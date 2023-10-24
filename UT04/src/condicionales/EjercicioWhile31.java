69239695040ec8e79967d003bde8891f45e82082
Natasha <nars.0596@gmail.com>
2023-10-24 17:23:17 +0200

    fin ejercicio 31

--- UT04/src/condicionales/EjercicioWhile31.java
+++ UT04/src/condicionales/EjercicioWhile31.java
@@ -8,27 +8,31 @@ public class EjercicioWhile31 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
-		Random rnd = new Random();
-		
-		
-		int numeroSecreto = rnd.nextInt(1,100);
-		System.out.println("Adivina el número secreto" + numeroSecreto);
-		numeroSecreto = scanner.nextInt();
-		
-		int intentosRestantes = 0;
-		for (int intentosRestante = 10; (intentosRestantes > 0) && (intentosRestante != numeroSecreto); intentosRestantes--) { 
-		System.out.println("El número introducido no es correcto, es mayor que el número introducido");
-		 
-		
-		
-		System.out.println("Has adivinado, ¡Ganaste!");
-		
-		
-		
-		
-		
-	
-	}
 
-}
+		Random rnd = new Random();
+		int numeroSecreto = rnd.nextInt(1, 100);
+		System.out.println("El número que hay que adivinar es el  " + numeroSecreto);
+
+		int numero = 0;
+
+		int intentosRestantes;
+		for (intentosRestantes = 10; (intentosRestantes > 0)
+				&& (numero != numeroSecreto); intentosRestantes--) {
+			System.out.printf("Introduce un número (quedan %d intentos): \n", intentosRestantes);
+			numero = scanner.nextInt();
+
+			if (numeroSecreto < numero) {
+				System.out.println("El número secerto es menor");
+			} else if (numeroSecreto > numero) {
+				System.out.println("El numero secreto es mayor");
+			}
+		}
+		if (numero == numeroSecreto) {
+			System.out.printf("Has adivinado, acertaste en %d intentos. \n ¡Ganaste!", 10 - intentosRestantes);
+		} else {
+			System.out.printf("No has acertado. Era %d. \n ", numeroSecreto);
+		
+		}
+
+	}
 }
---
+++ UT04/src/condicionales/EjercicioWhile32.java
@@ -0,0 +1,10 @@
+package condicionales;
+
+public class EjercicioWhile32 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
