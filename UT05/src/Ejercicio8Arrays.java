6e69f5ba0ad514850fb345d7e9db1a132fbf7425
Natasha <nars.0596@gmail.com>
2023-11-14 18:57:01 +0100

    8 y9

--- UT05/src/Ejercicio8Arrays.java
+++ UT05/src/Ejercicio8Arrays.java
@@ -1,9 +1,35 @@
+import java.util.Random;
 
 public class Ejercicio8Arrays {
 
+	private static final int CANTIDAD_NUMEROS = 20;
+	private static final int MIN = 20;
+	private static final int MAX = 20;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int numeros = 0;
+		int
+		llenarConAleatorios(numeros, MIN, MAX);
+		
+		
 	}
-
+	
+	private static void llenarConAleatorios (int[] numeros, int min, int max) {
+		
+		Random rnd = new Random ();
+		for (int i = 0; i < numeros.length; i++) {
+			numeros[i] =rnd.nextInt(min, max);
+			System.out.println("Numeros random");
+		} 
+}
+	private static int calcularSumaPosicionesImpares (int[] numeros) {
+		int  suma =0;
+		
+		for (int i = 0; i < numeros.length; i+=2) {
+			suma += numeros[i];
+			
+		}
+		return suma;
+	}
 }
--- UT05/src/Ejercicio9Arrays.java
+++ UT05/src/Ejercicio9Arrays.java
@@ -1,9 +1,43 @@
+import java.util.Random;
 
 public class Ejercicio9Arrays {
 
+	private static final int CANTIDAD_NUMEROS = 100;
+	private static final int MIN = 20;
+	private static final int MAX = 20;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int [] num = new int [CANTIDAD_NUMEROS];
+		
+		llenarConAleatorios (num, MIN, MAX);
+		mostrarNumeros(num);
+		procesarNumeros(num);
+		esPar(num);
+	
+	}
 
+	private static boolean esPar(int[] num) {
+		return false;
+		
+		
+		
+	}
+
+	private static void mostrarNumeros(int[] num) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void procesarNumeros(int[] num) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+
+	private static void llenarConAleatorios(int[] num, int min, int max) {
+		// TODO Auto-generated method stub
+		Random rnd = new Random ();
 	}
 
 }
