c23018964da701999c5a60e9a18c7e4122364ff4
Natasha <nars.0596@gmail.com>
2023-11-14 17:26:14 +0100

    8 y 9

--- UT05/src/Ejercicio7Arrays.java
+++ UT05/src/Ejercicio7Arrays.java
@@ -5,7 +5,16 @@ public class Ejercicio7Arrays {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] numeros = new int[20];
-		int rnd = 20;
+
+	}
+
+
+	private static boolean determinaSiUnNumeroEsPrimoONo (boolean numeros) {
+		Random rnd = new Random();
+		
+		
+		return numeros;
+		
 	}
 
 }
