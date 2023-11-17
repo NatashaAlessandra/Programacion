29fa7a170447cdf6d3dd7114ea52ab59e048ab61
NatashaAlessandra <147174988+NatashaAlessandra@users.noreply.github.com>
2023-11-17 10:04:07 +0100

    Ejercicio 3 y 4

--- UT05/src/Ejercicio3Array.java
+++ UT05/src/Ejercicio3Array.java
@@ -1,60 +1,54 @@
-//import java.util.Iterator;
-//import java.util.Scanner;
-////
-//public class Ejercicio3Array {
-//
-//	public static void main(String[] args) {
-//		// TODO Auto-generated method stub
-//		Scanner scanner = new Scanner(System.in);
-//		int numElementos = pedirCantidadNumeros(scanner);
-//
-//		int[] numeros = new int[numElementos];
-//
-//		pedirNumeros(numeros, scanner);
-//
-//////		mostrarNumerosOredenNormal(numElementos);
-//////
-//////		mostrarNumerosOrdenInverso(numElementos);
-////	}
-////
-////	public static int pedirCantidadNumeros(Scanner scanner) {
-////		int cantidad = 0;
-//
-//		System.out.println("Introduce un numero");
-//		do  {
-//			System.out.println("El numero tiene que ser mayor que 0");
-//			cantidad = scanner.nextInt();
-//			if (cantidad <= 0) {
-//				System.out.println("Introduce un m");
-////			}
-//		
-//
-////		return cantidad;
-//
-//	}
-//
-//	public static void pedirNumeros(int[] numeros, Scanner scanner) {
-//		for (int i = 0; i < numeros.length; i++) {
-//			System.out.printf("Introduce el numero para la posición %d: ", i);
-//			numeros[i] = scanner.nextInt();
-//		}
-//	}
-//
-////	public static int mostrarNumerosOredenNormal (int numerosOrden) {
-////		for (int i < numeros.length -1; 1 > 0; i++) {
-////			int numero =
-////			
-////		}
-////		return numerosOrden;
-////		
-////	}
-////
-////	public static int mostrarNumerosOrdenInverso(int numerosInversos) {
-////		for (int i < numeros.length -1; i >=0; i++) {
-////				int numero =numeros [i]
-////
-////		}
-////		return numerosInversos;
-////
-////	}
-//}
+import java.util.Scanner;
+
+public class Ejercicio3Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+
+		int[] numeros = new int[pedirCantidadNumeros(scanner)];
+		pedirNumeros(scanner, numeros);
+		mostrarNumerosOrdenNormal(numeros);
+		mostrarNumerosOrdenInverso(numeros);
+	}
+
+	private static void mostrarNumerosOrdenInverso( int[] numeros) {
+		// TODO Auto-generated method stub
+		for (int i = numeros.length-1; i >= 0; i--) {
+			System.out.printf("%d ", numeros[i]);
+		}
+			System.out.println();
+
+	}
+
+	private static void mostrarNumerosOrdenNormal( int[] numeros) {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < numeros.length; i++) {
+			System.out.printf("%d ", numeros[i]);
+		}
+		System.out.println();
+	}
+
+	private static void pedirNumeros(Scanner scanner, int[] numeros) {
+
+		for (int i = 0; i < numeros.length; i++) {
+			System.out.println("Introduce números");
+			numeros[i]= scanner.nextInt();
+
+		}
+		
+	}
+
+	private static int pedirCantidadNumeros(Scanner scanner) {
+
+		System.out.println("Cuántos números vas a introducir?");
+		int cantidad = scanner.nextInt();
+		while (cantidad <= 0) {
+			System.out.println("El número introducido es 0 o menor. Cuántos números vas a introducir?");
+			cantidad = scanner.nextInt();
+		}
+
+		return cantidad;
+
+	}
+}
--- UT05/src/Ejercicio4Arrays.java
+++ UT05/src/Ejercicio4Arrays.java
@@ -1,9 +1,21 @@
+import java.util.Random;
+import java.util.Scanner;
 
 public class Ejercicio4Arrays {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Random rnd = new Random();
+		int[] numeros = new int[10];
+		generarNumeros(rnd, numeros);
 	}
 
+	private static void generarNumeros(Random rnd, int[] numeros) {
+
+		for (int i = 0; i < numeros.length; i++) {
+			numeros[i] = rnd.nextInt(0, 99);
+			System.out.printf("%d\n", numeros[i]);
+
+		}
+	}
 }
