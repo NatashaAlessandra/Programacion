54227cebd795d0ac7d3c6461185a71859e01e734
NatashaAlessandra <147174988+NatashaAlessandra@users.noreply.github.com>
2023-11-17 11:45:00 +0100

    Ejercicio 4 completado

--- UT05/src/Ejercicio4Arrays.java
+++ UT05/src/Ejercicio4Arrays.java
@@ -1,3 +1,4 @@
+import java.util.Iterator;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -5,17 +6,38 @@ public class Ejercicio4Arrays {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Random rnd = new Random();
+		Random rnd = new Random(1);
 		int[] numeros = new int[10];
 		generarNumeros(rnd, numeros);
+		int suma = calcularSuma(numeros);
+		calularMedia(suma, numeros);
 	}
 
 	private static void generarNumeros(Random rnd, int[] numeros) {
 
 		for (int i = 0; i < numeros.length; i++) {
-			numeros[i] = rnd.nextInt(0, 99);
-			System.out.printf("%d\n", numeros[i]);
+			numeros[i] = rnd.nextInt(0, 9);
+			System.out.printf("%d ", numeros[i]);
 
 		}
+		System.out.println();
+
+	}
+
+	private static int calcularSuma(int[] numeros) {
+		int suma = 0;
+		for (int i = 0; i < numeros.length; i++) {
+			suma = numeros[i] + suma;
+
+		}
+		System.out.printf("Suma = %d ", suma);
+		return suma;
+	}
+
+	private static double calularMedia(int suma, int[] numeros) {
+		double mediaNum = 0;
+		mediaNum = (double)suma / numeros.length;
+		System.out.printf("Media = %.2f ", mediaNum);
+		return mediaNum;
 	}
 }
