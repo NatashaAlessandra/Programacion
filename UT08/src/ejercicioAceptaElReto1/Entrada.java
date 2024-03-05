2cd0a15a85affcb695d38eda83e13e26b6e4d321
Natasha <nars.0596@gmail.com>
2024-03-05 18:16:36 +0100

    update acepta el reto

--- UT08/src/ejercicioAceptaElReto1/Entrada.java
+++ UT08/src/ejercicioAceptaElReto1/Entrada.java
@@ -1,5 +1,26 @@
 package ejercicioAceptaElReto1;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Entrada {
 
+	Scanner sc = new Scanner(System.in);
+	int numNiños;
+	int numRegalos;
+
+	public int pedirNumeroNiños() {
+		System.out.println("Cuántos niños están en la fila");
+		numNiños = sc.nextInt();
+		return numNiños;
+	}
+
+	public int pedirNumeroRegalos() {
+		System.out.println("Cuántos niños están en la fila");
+		numRegalos = sc.nextInt();
+		return numRegalos;
+
+	}
+	
+	ArrayList<Integer> colaDeRegalos = new ArrayList<>();
 }
